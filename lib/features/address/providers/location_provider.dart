ac20830dce6956f609f255ea49ca2968fc22f233
onecloudappsllc <oneclouadppsllc@gmail.com>
2026-03-01 10:13:48 +0200

    Remove unused google_maps_webservice import

--- lib/features/address/providers/location_provider.dart
+++ lib/features/address/providers/location_provider.dart
@@ -13,7 +13,6 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:google_maps_webservice/places.dart';
 import 'package:http/http.dart' as http;
 class LocationProvider with ChangeNotifier {
   final SharedPreferences? sharedPreferences;
