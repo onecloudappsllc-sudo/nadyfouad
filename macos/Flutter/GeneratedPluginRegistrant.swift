0a249f0c27c2c7b6127345e78b6399ad841d800b
onecloudappsllc <oneclouadppsllc@gmail.com>
2026-05-20 02:05:14 +0300

    update pubspec.lock

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,7 +22,7 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import webview_flutter_wkwebview
 
